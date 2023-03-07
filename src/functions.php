3849bba0962c56753e7b400f3b8f475b734890a6
Larsen <john.larsen@mittanbud.no>
2023-03-07 11:54:47 +0100

    Added php tags to class code. More clean.

--- src/functions.php
+++ src/functions.php
@@ -2,7 +2,7 @@
 
 function arrayOf(string $type, bool $cache = false): Thakladd\Phenerics\Collection {
     $class_name = 'ArrayOf' . $type;
-    $class = 'class ' . $class_name . ' extends Thakladd\Phenerics\Collection {}';
+    $class = '<?php class ' . $class_name . ' extends Thakladd\Phenerics\Collection {} ?>';
     if (!class_exists($class_name)) {
         if (!checkClassName($type)) {
             throw new Thakladd\Phenerics\Exceptions\TypeNotValidException('Type "' . $type . '" is not a valid string.');
@@ -19,7 +19,7 @@ function arrayOf(string $type, bool $cache = false): Thakladd\Phenerics\Collecti
 function storeClass(string $class_name, string $class) {
     $filename = __DIR__ . '/cache/' . $class_name . '.php';
     if (!is_file($filename)) {
-        file_put_contents($filename, '<?php' . PHP_EOL . $class);
+        file_put_contents($filename, $class);
     }
     require_once($filename);
     return true;
